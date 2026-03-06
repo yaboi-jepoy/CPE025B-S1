14d2f17873d1e979edde798b8e7e27aaa72571db
yaboi-jepoy <qjpapenas@tip.edu.ph>
2026-03-06 16:16:21 +0800

    HoA 8.1

---
+++ module8/hoa8_1.js
@@ -0,0 +1,65 @@
+class Vehicle {
+  constructor(id, type) {
+    // Code here!
+    this.id = id;
+    this.type = type;
+    this.isDeployed = false;
+  }
+}
+class FleetManager {
+  constructor() {
+    this.vehicles = [];
+  }
+  addVehicle(vehicle) {
+    this.vehicles.push(vehicle);
+  }
+  deployVehicle(id) {
+    //? HOW DO I ADD UNDEFINED HEERE?
+    for (let vehicle in this.vehicles) {
+      if (this.vehicles[vehicle].id == id) {
+        this.vehicles[vehicle].isDeployed = true;
+        // console.log(`${this.vehicles[vehicle].id}`);
+      }
+    }
+  }
+  getAvailableVehicles() {
+    this.availableVehicles = [];
+    for (let vehicle in this.vehicles) {
+      if (this.vehicles[vehicle].isDeployed === false) {
+        this.availableVehicles.push(this.vehicles[vehicle]);
+      }
+    }
+    return this.availableVehicles;
+  }
+}
+
+//! DO NOT MODIFY
+// Test Entity Instantiation
+console.log("*** Test Entity Instantiation***");
+const testVehicle = new Vehicle("V01", "Truck");
+console.log(testVehicle);
+// Expect Vehicle object with id: V01, type: Truck, isDeployed: false
+
+// Test Composition and Insertion
+console.log("\n*** Test Composition and Insertion***");
+const fleet = new FleetManager();
+fleet.addVehicle(testVehicle);
+fleet.addVehicle(new Vehicle("V02", "Van"));
+fleet.addVehicle(new Vehicle("V03", "Drone"));
+console.log(fleet.vehicles.length);
+// Expect: 3
+
+// Test State Mutation
+console.log("\n*** Test State Mutation ***");
+fleet.deployVehicle("V02");
+const deployedVehicle = fleet.vehicles.find((v) => v.id === "V02");
+console.log(deployedVehicle.isDeployed);
+// Expect: true
+
+// Test Data Filtering
+console.log("\n*** Test Data Filtering ***");
+const available = fleet.getAvailableVehicles();
+console.log(available.length);
+// Expect: 2
+console.log(available.map((v) => v.id));
+// Expect: [ V01, V03 ]
